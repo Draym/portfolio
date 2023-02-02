cfae6211d4146240adcb4b8ab248e2b3e10ccc77
Draym <kevin.andres@epitech.eu>
2023-02-02 12:05:40 +0800

    task: update profile section

--- src/views/view/sections/SectionProfile.js
+++ src/views/view/sections/SectionProfile.js
@@ -22,13 +22,13 @@ class SectionProfile extends Component {
                         </div>
                         <div className="col-12 col-md-9 profile-content">
                             <p className="p-title">Hi! I'm Kevin</p>
-                            <p>A French Software engineer with 4+ years of enterprise experience in full-stack development, mainly using Java/NodeJs
-                                and ReactJs while participating in the complete product development lifecycle within Agile teams. Specializing
+                            <p>A French Software engineer with 5+ years of enterprise experience in full-stack development, mainly using Java/Kotlin/Node
+                                and React while participating in the complete product development lifecycle. I have experience interacting with stakeholders and PM about product and system design. I am specializing
                                 in backend development and micro-services architecture on cloud services.</p>
                             <p className="mb-1">Animated by well-designed code and discovering new business problematics; motivated by novelty and tackle
                                 technical & design challenges.</p>
-                            <p>Passionate in SaaS, Blockchain, Multi-player games, AI Robotics, Finance and Data analysis.</p>
-                            <p>I'm currently loving Kotlin and ReactJs and testing myself on blockchain projects. Soon to come Go.</p>
+                            <p>Passionate in SaaS, Web Applications, Blockchain, Decentralised Applications, Multi-player games, and Finance.</p>
+                            <p>I'm currently really interested by blockchain and experiencing it on personal projects on <a href="https://github.com/Draym" target="_blank">github</a>.</p>
                             <p className="pt-3">Feel free to <AnchorLink href="#contact">contact me</AnchorLink> for opportunities.</p>
                         </div>
                     </div>
